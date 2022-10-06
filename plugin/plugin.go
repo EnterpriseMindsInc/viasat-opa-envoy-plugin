c4a7e3a77ebacc006f9dfc88c9f7b2719b182fc2
Dinesh Sinnarasse <dinesh@eminds.ai>
2022-10-06 14:58:58 +0530

    Added necessary files to develop custom flow.

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,27 +1,28 @@
+package plugin
+
 // Copyright 2020 The OPA Authors. All rights reserved.
 // Use of this source code is governed by an Apache2
 // license that can be found in the LICENSE file.
 
-package plugin
-
 import (
 	"github.com/open-policy-agent/opa/plugins"
 
-	"github.com/open-policy-agent/opa-envoy-plugin/internal"
+	"github.com/open-policy-agent/opa-envoy-plugin/inbound"
 )
 
 // Factory defines the interface OPA uses to instantiate a plugin.
 type Factory struct{}
 
 // PluginName is the name to register with the OPA plugin manager
-const PluginName = internal.PluginName
+// const PluginName = internal.PluginName
+const PluginName = inbound.PluginName
 
 // New returns the object initialized with a valid plugin configuration.
 func (Factory) New(m *plugins.Manager, config interface{}) plugins.Plugin {
-	return internal.New(m, config.(*internal.Config))
+	return inbound.New(m, config.(*inbound.Config))
 }
 
 // Validate returns a valid configuration to instantiate the plugin.
 func (Factory) Validate(m *plugins.Manager, config []byte) (interface{}, error) {
-	return internal.Validate(m, config)
+	return inbound.Validate(m, config)
 }
